f94f2fd9efaa851472ac04620030e369e098123a
Neha Panbude <nehapanbude1904@gmail.com>
2025-09-21 18:34:03 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+Simple entry point script to run the Burger Billing System.
+"""
+
+from main import main
+
+# Run the application
+if __name__ == "__main__":
+    main()
